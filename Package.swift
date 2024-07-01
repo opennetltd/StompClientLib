366dc8dd168205c78da7288bd9b1529343f4fad4
Chirag Lakhani <chirag.lakhani@apertumo.com>
2024-07-01 09:46:29 +0530

    socket rocket library update

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["StompClientLib"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/opennetltd/SocketRocket.git", .exact("1.0.0")),
+        .package(url: "https://github.com/opennetltd/SocketRocket.git", .exact("1.0.1-SNAPSHOT")),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
